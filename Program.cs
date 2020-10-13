493423b72d678cf265bccf8e3c2d1cadc13d70ca
SpencerLeBard <slebard@gmail.com>
2020-10-13 13:47:29 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeLibrary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(@"
+   __ _ _                          
+  / /(_) |__  _ __ __ _ _ __ _   _ 
+ / / | | '_ \| '__/ _` | '__| | | |
+/ /__| | |_) | | | (_| | |  | |_| |
+\____/_|_.__/|_|  \__,_|_|   \__, |
+                             |___/
+      ");
+    }
+  }
+}
